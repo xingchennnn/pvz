39d82835aef23c843c59b3e57d71b45d0e7af0e3
raok <2955095425@qq.com>
2025-08-27 18:41:52 +0800

    docs(ui): 更新窗口列表显示样式和功能
    新增窗口列表标题，并改进窗口列表的显示样式，使用更详细的格式展示每个窗口的信息，包括标题、类名和句柄。
    新增点击事件以切换窗口的激活状态，虽然目前功能被注释掉，但已实现状态切换逻辑。

--- src/views/windowPos/index.vue
+++ src/views/windowPos/index.vue
@@ -1,27 +1,60 @@
 <template>
-  <div>{{ windows }}</div>
+  <div>
+    <h2 class="text-lg font-bold mb-2">窗口列表</h2>
+    <ul class="list-none p-0">
+      <li
+        v-for="(win, index) in windows"
+        :key="index"
+        :class="`border-1 
+         bg-[var(--text-color)] 
+        c-[var(--bg-color)] p-1 mb-2 rounded 
+        flex flex-col cursor-pointer 
+        hover:bg-[#f0f0f0] 
+        ${win.isActive ? 'border-[#007bff]' : ''}
+        `"
+        @click="setActiveWindow(win)"
+      >
+        <span>标题: {{ win.title }}</span>
+        <span>类名: {{ win.class_name }}</span>
+        <span>句柄: {{ win.hwnd }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 
-const windows = ref<string[]>([]);
+const windows = ref<
+  { title: string; class_name: string; hwnd: number; isActive: boolean }[]
+>([]);
 
 // 请求所有窗口
 const getAllWindows = async () => {
   const JsonData = await invoke<string>("get_all_windows");
-
   let windows = JSON.parse(JsonData);
-  // windows.forEach((win: any) => {
-  //   win = JSON.parse(win);
-  // });
+
   windows = windows.filter((win: any) => {
-    return !["IME", "MSCTFIME UI", "BroadcastListenerWindow"].includes(win.class_name);
+    return !["IME", "MSCTFIME UI", "BroadcastListenerWindow"].includes(
+      win.class_name
+    );
   });
-  console.log('传递了', windows.length);
 
   return windows;
+};
+
+const setActiveWindow = (win: any) => {
+  win.isActive = !win.isActive;
+  // invoke("set_window_pos", {
+  //   hwnd: win.hwnd,
+  //   insertAfter: win.isActive ? -1 : -2, // -1 置顶 -2 取消置顶
+  //   x: 0,
+  //   y: 0,
+  //   cx: 0,
+  //   cy: 0,
+  //   uFlags: 3, // SWP_NOSIZE | SWP_NOMOVE
+  // });
 };
 
 onMounted(async () => {
